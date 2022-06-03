3979ec8ab29bbe4d7a80860d68b31fc8783a37e6
Arif Setiawan <ariffsetiawan@gmail.com>
2022-06-03 18:52:33 +0700

    chore: add google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,26 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Script from 'next/script';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+      <>
+        <Script strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=G-CWPNJVJZKD`} />
+
+        <Script strategy="lazyOnload">
+            {`
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'G-CWPNJVJZKD', {
+                page_path: window.location.pathname,
+                });
+            `}
+        </Script>
+
+        <Component {...pageProps} />
+      </>
+  );
 }
 
 export default MyApp
